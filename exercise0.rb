fbfe43d5a2bc80a951c26a13da460ab5260ed999
StephenVarela <varelast92@gmail.com>
2018-09-06 14:08:08 -0400

    created variables used for following exercises

---
+++ exercise0.rb
@@ -0,0 +1,35 @@
+fav_colors = ['red', 'blue', 'green', 'yellow']
+ages = [22,24,32,19,7]
+coin_flips = ['heads', 'tails', 'heads', 'heads', 'heads']
+fav_artists = ['Dream Theater', 'Metallica', 'Motley Crue']
+fav_colors_sym = [:red, :blue, :green, :yellow]
+
+# Create a hash for each item below that contains the given information:
+#
+# three words and their definitions
+# your favourite movie names and their year of creation
+# three cities of the world and their population
+# the names of your siblings/cousins/friends and their age
+dictionary = {
+  :search => 'to go or look through a place to find something missing or lost',
+  :dive => 'to plunge into water head first',
+  :study => 'application of the mind to the aquisition of knowledge'
+}
+
+movies = {
+  :Lord_of_the_Rings => 2001,
+  :Star_Wars => 2008,
+  :Inception => 2010
+}
+
+cities = {
+  :Toronto => 2809000,
+  :Perth => 1671000,
+  :Mumbai => 18410000
+}
+
+relatives = {
+  :Simonne => 21,
+  :Shannon => 24,
+  :Joshua => 18
+}
