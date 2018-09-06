68964161bf593d12bb55ff2f555af7aadc6fbd63
StephenVarela <varelast92@gmail.com>
2018-09-06 17:14:58 -0400

    created pizza topping hash polling users

---
+++ exercise12.rb
@@ -0,0 +1,20 @@
+
+
+
+
+##prompt user for number of pizzas
+puts "How many pizzas do you want to order?"
+number_of_pizzas = gets.chomp.to_i
+
+pizza_order = {}
+
+number_of_pizzas.times do |pizza_number|
+    puts "How many toppings for pizza #{pizza_number+1}"
+    toppings = gets.chomp.to_i
+    pizza_order[pizza_number] = toppings
+end
+
+puts "Your order is as follows: "
+pizza_order.each do |pizza_number, toppings|
+  puts "pizza_number #{pizza_number+1} has #{toppings+1} toppings"
+end
